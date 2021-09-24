f583f038015375abcccdad651abb13d6ee0fab39
Полина <bugoaneo@users.noreply.github.com>
2021-09-24 10:59:28 +0700

    RosSite

---
+++ rossite/js/scripts.js
@@ -0,0 +1,16 @@
+// (function () {
+
+//  const cropElement = document.querySelectorAll('.card__text'),
+//   size = 257,
+//   endCharacter = '...';
+
+//  cropElement.forEach(el => {
+//   let text = el.innerHTML;
+
+//   if (el.innerHTML.length > size) {
+//    text = text.substr(0, size);
+//    el.innerHTML = text + endCharacter;
+//   }
+//  });
+
+// }());
